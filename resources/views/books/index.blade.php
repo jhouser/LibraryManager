c80f07328153f6c15368675a5c91639b9621ec40
Jake Houser <jakob.houser@gmail.com>
2017-09-10 11:00:58 +0200

    Add styling to books index page

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -1,16 +1,24 @@
 @extends('app')
 @section('title', 'Books')
 @section('content')
-<table>
-    <tr>
-        <th>Title</th>
-        <th>Authors</th>
-    </tr>
-    @foreach ($books as $book)
-        <tr>
-            <td>{{ $book->name }}</td>
-            <td>{{ $book->formatAuthors() }}</td>
-        </tr>
-    @endforeach
-</table>
+<div class="col-md-12">
+    <table class="table table-bordered table-hover table-responsive">
+        <thead>
+            <tr>
+                <th>Title</th>
+                <th>Authors</th>
+                <th>Delete</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($books as $book)
+            <tr>
+                <td>{{ $book->name }}</td>
+                <td>{{ $book->formatAuthors() }}</td>
+                <td><button class="btn btn-default"><span class="glyphicon glyphicon-trash"></span></button></td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
 @stop
