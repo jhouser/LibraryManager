0f9811a0800335ebb96883d3b2e81ff67ebc4a16
Jake Houser <jakob.houser@gmail.com>
2017-09-11 19:38:04 +0200

    Add error messages to book form

--- resources/views/books/form.blade.php
+++ resources/views/books/form.blade.php
@@ -3,17 +3,19 @@
         {{csrf_field()}}
         <div class="row">
             <div class="col-md-12">
-                <div class="form-group col-md-8">
+                <div class="form-group col-md-8 {{ $errors->has('title') ? 'has-error' : '' }}">
                     <label for="title">Title: </label>
                     <input type="text"class="form-control" name="title" />
+                    {!! $errors->first('title', '<p class="help-block">:message</p>') !!}
                 </div>
             </div>
         </div>
         <div class="row">
             <div class="col-md-12">
-                <div class="form-group col-md-8">
-                    <label for="authors">Author:</label>
+                <div class="form-group col-md-8 {{ $errors->has('author') ? 'has-error' : '' }}">
+                    <label for="author">Author:</label>
                     <input type="text"class="form-control" name="author"/>
+                     {!! $errors->first('author', '<p class="help-block">:message</p>') !!}
                 </div>
             </div>
         </div>
