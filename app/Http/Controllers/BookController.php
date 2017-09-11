063e3bc98b28c895843bd76bf7aa6a751ef46b9d
Jake Houser <jakob.houser@gmail.com>
2017-09-11 18:27:39 +0200

    Delete unused methods

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -52,29 +52,6 @@ class BookController extends Controller
     }
 
     /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Book  $book
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Book $book)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Book  $book
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Book $book)
-    {
-        //
-    }
-
-    /**
      * Remove the specified resource from storage.
      *
      * @param  \App\Book  $book
